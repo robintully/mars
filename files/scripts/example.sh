d412834084392088b2c69e98bc35218dbbb31ec5
Robin Tully <mr.robintully@gmail.com>
2024-08-19 10:46:59 -0700

    adding cinnamon fallback

--- files/scripts/example.sh
+++ files/scripts/example.sh
@@ -5,7 +5,7 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-dnf group install -y "Budgie Desktop"
+dnf group install -y "Cinnamon Desktop"
 # Your code goes here.
 echo 'This is an example shell script'
 echo 'Scripts here will run during build if specified in recipe.yml'
